Fix rotation-based metrics computing

scripts/interpolation_test.py
@@ -10,7 +10,7 @@ from scipy.spatial.transform import Rotation as R
 from utils.dataset import BvhDataset
 from utils.interpolation import interpolate_positions, interpolate_rotations
 from utils.utils import forward_kinematics, load_params_from_json
-from utils.rotation_convertion import rot6d_to_mat_torch
+from utils.rotation_convertion import make_quaternions_continous, mat_to_quat_torch, rot6d_to_mat_torch
 from model.model import MotionTransformer
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -120,14 +120,20 @@ def test_and_get_scores(
             )
 
             # Compute gt global quaternions for L2Q metric
-            gt_rot_mats_fk_np = gt_rot_mats_fk.cpu().numpy().reshape(-1, 3, 3)
-            gt_rot_q_fk_np = R.from_matrix(gt_rot_mats_fk_np).as_quat()
-            gt_rot_fk_q = torch.tensor(gt_rot_q_fk_np, device=DEVICE).view(B, T, J, 4)
+            # gt_rot_mats_fk_np = gt_rot_mats_fk.cpu().numpy().reshape(-1, 3, 3)
+            # gt_rot_q_fk_np = R.from_matrix(gt_rot_mats_fk_np).as_quat()
+            # gt_rot_fk_q = torch.tensor(gt_rot_q_fk_np, device=DEVICE).view(B, T, J, 4)
 
             # Compute predicted global quaternions for L2Q metric
-            pred_rot_mats_fk_np = pred_rot_mats_fk.cpu().numpy().reshape(-1, 3, 3)
-            pred_rot_q_fk_np = R.from_matrix(pred_rot_mats_fk_np).as_quat()
-            pred_rot_fk_q = torch.tensor(pred_rot_q_fk_np, device=DEVICE).view(B, T, J, 4)
+            # pred_rot_mats_fk_np = pred_rot_mats_fk.cpu().numpy().reshape(-1, 3, 3)
+            # pred_rot_q_fk_np = R.from_matrix(pred_rot_mats_fk_np).as_quat()
+            # pred_rot_fk_q = torch.tensor(pred_rot_q_fk_np, device=DEVICE).view(B, T, J, 4)
+
+            gt_rot_fk_q = mat_to_quat_torch(gt_rot_mats_fk)
+            pred_rot_fk_q = mat_to_quat_torch(pred_rot_mats_fk)
+
+            gt_rot_fk_q = make_quaternions_continous(gt_rot_fk_q)
+            pred_rot_fk_q = make_quaternions_continous(pred_rot_fk_q)
 
             # --- METRICS ---
 

test.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from utils.dataset import BvhDataset
 from utils.interpolation import interpolate_positions, interpolate_rotations
 from utils.utils import forward_kinematics, load_params_from_json, set_seed
-from utils.rotation_convertion import mat_to_quat_torch, rot6d_to_mat_torch
+from utils.rotation_convertion import make_quaternions_continous, mat_to_quat_torch, rot6d_to_mat_torch
 from model.model import MotionTransformer
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -171,6 +171,9 @@ def test_and_get_scores(
 
             gt_rot_fk_q = mat_to_quat_torch(gt_rot_mats_fk)
             pred_rot_fk_q = mat_to_quat_torch(pred_rot_mats_fk)
+
+            gt_rot_fk_q = make_quaternions_continous(gt_rot_fk_q)
+            pred_rot_fk_q = make_quaternions_continous(pred_rot_fk_q)
 
             # --- METRICS ---
 

utils/metrics.py
@@ -20,21 +20,24 @@ def npss(pred_seq, gt_seq):
     EPS = 1e-8
 
     # FFT along the time dimension
-    gt_fft = torch.fft.rfft(gt_seq, dim=1)
-    pred_fft = torch.fft.rfft(pred_seq, dim=1)
+    gt_fft = torch.fft.rfft(gt_seq, dim=1)      # [B, T//2 + 1, D]
+    pred_fft = torch.fft.rfft(pred_seq, dim=1)  # [B, T//2 + 1, D]
     
     # Power spectrum
-    gt_power = torch.abs(gt_fft) ** 2
-    pred_power = torch.abs(pred_fft) ** 2
+    gt_power = torch.abs(gt_fft) ** 2           # [B, T//2 + 1, D]
+    pred_power = torch.abs(pred_fft) ** 2       # [B, T//2 + 1, D]
 
     # Normalize power spectrum
-    gt_power_norm = gt_power / (torch.sum(gt_power, dim=1, keepdim=True) + EPS)
-    pred_power_norm = pred_power / (torch.sum(pred_power, dim=1, keepdim=True) + EPS)
+    gt_power_norm = gt_power / (torch.sum(gt_power, dim=1, keepdim=True) + EPS)         # [B, T//2 + 1, D]
+    pred_power_norm = pred_power / (torch.sum(pred_power, dim=1, keepdim=True) + EPS)   # [B, T//2 + 1, D]
+    
+    gt_power_cdf = torch.cumsum(gt_power_norm, dim=1)       # [B, T//2 + 1, D]
+    pred_power_cdf = torch.cumsum(pred_power_norm, dim=1)   # [B, T//2 + 1, D]
 
-    emd = torch.sum(torch.abs(gt_power_norm - pred_power_norm), dim=1)
-    p = torch.sum(gt_power, dim=1)
+    emd = torch.sum(torch.abs(gt_power_cdf - pred_power_cdf), dim=1)  # [B, D]
+    p = torch.sum(gt_power, dim=1)                                    # [B, D]
 
     # Compute NPSs score
-    npss_score = torch.sum(p * emd, dim=1) / (torch.sum(p, dim=1) + EPS)
-    return npss_score.mean().item()
+    npss_score = torch.sum(torch.sum(p * emd, dim=1), dim=0) / (torch.sum(torch.sum(p, dim=1), dim=0) + EPS)
+    return npss_score.item()
 #npss